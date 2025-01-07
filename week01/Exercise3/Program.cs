fa18d0a0526826104b45c4e93ab985ab6c00cd64
maz131212 <mazariegosax@gmail.com>
2025-01-07 16:14:01 -0600

    w1-ex3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,57 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        //Core requirement #1 
+        Console.WriteLine("");
+        // Console.Write("What is the magic number? ");
+        // string randomNumber = Console.ReadLine();
+        // int magicNumber = int.Parse(randomNumber);
+
+        
+        //Stretch Challenge #2
+        string play = "yes";
+
+        do
+        {
+            //Core requirement #3
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+            int guessNumber = 0;
+            int attempts = 0;
+            
+            //Core requirement #2
+            do
+            {
+                Console.Write("What is your guess? ");
+                string guess = Console.ReadLine();
+                guessNumber = int.Parse(guess);
+
+                if (guessNumber > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guessNumber < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("You guessed it!");
+                }
+                attempts++;
+                
+            } while (guessNumber != magicNumber);
+
+            // Stretch Challenge #1
+            Console.WriteLine($"It took you {attempts} attempts to guess the magic number {magicNumber}");
+            Console.WriteLine("");
+
+            Console.Write("Do you want to play again? (yes/no) ");
+            play = Console.ReadLine();
+            Console.WriteLine("");
+
+        } while (play == "yes");
     }
+    
 }
