8a63c8e77054ed805333548152b0fb25cffcb373
maz131212 <mazariegosax@gmail.com>
2025-01-07 17:06:21 -0600

    w1-ex4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,72 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int number = 1;
+
+        do 
+        {
+            Console.Write("Enter number: ");
+                    string userInput = Console.ReadLine();
+                    number = int.Parse(userInput);
+                    if (number != 0)
+                    {
+                        numbers.Add(number);
+                    }
+        } while (number !=0);
+
+        // Core requirement #1
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        // Core requirement #2
+        float average = 0;
+        average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        // Core requirement #3
+        int max = 0;
+        foreach (int num in numbers)
+        {
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+        Console.WriteLine($"The largest number is: {max}");
+        
+        // Stretch Challenge #1
+        float smallest = float.PositiveInfinity;
+         foreach (int num in numbers)
+        {
+            if (num < smallest && num > 0)
+            {
+                smallest = num;
+            }
+        }
+        Console.WriteLine($"The smallest positive number is: {smallest}");
+
+        // Stretch Challenge #2
+        numbers.Sort();
+        Console.WriteLine("The sorted list is:");
+        foreach (int num in numbers)
+        {
+            Console.WriteLine(num);
+        }
+
+        Console.WriteLine("");
+
     }
 }
